4fbbaf6ea67243675de5d5112ab40616e5430e8b
PanchoEc <157312013+PanchoEc@users.noreply.github.com>
2024-02-10 19:19:04 -0500

    Add files via upload

---
+++ PIC-master/PIC/Vector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector.Desktop
+{
+    public abstract class Vector
+    {
+        private double _x;
+        private double _y;
+        private double _z;
+
+        public Vector(double x, double y, double z)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+
+        public double X { get => _x; set => _x = value; }
+        public double Y { get => _y; set => _y = value; }
+        public double Z { get => _z; set => _z = value; }
+        
+     }
+}
---
+++ PIC-master/Vectores.Desktop/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace Vectores.Desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            txtX1 = new TextBox();
+            txtY1 = new TextBox();
+            txtZ1 = new TextBox();
+            txtZ2 = new TextBox();
+            txtY2 = new TextBox();
+            txtX2 = new TextBox();
+            btnExit = new Button();
+            btnSaveReg = new Button();
+            error1 = new ErrorProvider(components);
+            label15 = new Label();
+            label16 = new Label();
+            txtAdd = new TextBox();
+            txtSubstract = new TextBox();
+            txtPointProduct = new TextBox();
+            label17 = new Label();
+            btnClear = new Button();
+            ((System.ComponentModel.ISupportInitialize)error1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(302, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(366, 28);
+            label1.TabIndex = 0;
+            label1.Text = "OPERACIONES CON VECTORES";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(237, 88);
+            label2.Name = "label2";
+            label2.Size = new Size(102, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Vector A = (x, y, z)";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(247, 123);
+            label3.Name = "label3";
+            label3.Size = new Size(24, 15);
+            label3.TabIndex = 2;
+            label3.Text = "x =";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(247, 147);
+            label4.Name = "label4";
+            label4.Size = new Size(24, 15);
+            label4.TabIndex = 3;
+            label4.Text = "y =";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(247, 179);
+            label5.Name = "label5";
+            label5.Size = new Size(23, 15);
+            label5.TabIndex = 4;
+            label5.Text = "z =";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(588, 179);
+            label6.Name = "label6";
+            label6.Size = new Size(23, 15);
+            label6.TabIndex = 8;
+            label6.Text = "z =";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(588, 147);
+            label7.Name = "label7";
+            label7.Size = new Size(24, 15);
+            label7.TabIndex = 7;
+            label7.Text = "y =";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(588, 123);
+            label8.Name = "label8";
+            label8.Size = new Size(24, 15);
+            label8.TabIndex = 6;
+            label8.Text = "x =";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(578, 88);
+            label9.Name = "label9";
+            label9.Size = new Size(101, 15);
+            label9.TabIndex = 5;
+            label9.Text = "Vector B = (x, y, z)";
+            // 
+            // txtX1
+            // 
+            txtX1.Location = new Point(299, 115);
+            txtX1.Name = "txtX1";
+            txtX1.Size = new Size(100, 23);
+            txtX1.TabIndex = 9;
+            // 
+            // txtY1
+            // 
+            txtY1.Location = new Point(299, 147);
+            txtY1.Name = "txtY1";
+            txtY1.Size = new Size(100, 23);
+            txtY1.TabIndex = 10;
+            // 
+            // txtZ1
+            // 
+            txtZ1.Location = new Point(299, 179);
+            txtZ1.Name = "txtZ1";
+            txtZ1.Size = new Size(100, 23);
+            txtZ1.TabIndex = 11;
+            // 
+            // txtZ2
+            // 
+            txtZ2.Location = new Point(623, 179);
+            txtZ2.Name = "txtZ2";
+            txtZ2.Size = new Size(100, 23);
+            txtZ2.TabIndex = 14;
+            // 
+            // txtY2
+            // 
+            txtY2.Location = new Point(623, 147);
+            txtY2.Name = "txtY2";
+            txtY2.Size = new Size(100, 23);
+            txtY2.TabIndex = 13;
+            // 
+            // txtX2
+            // 
+            txtX2.Location = new Point(623, 115);
+            txtX2.Name = "txtX2";
+            txtX2.Size = new Size(100, 23);
+            txtX2.TabIndex = 12;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(834, 393);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(75, 23);
+            btnExit.TabIndex = 17;
+            btnExit.Text = "Salir";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // btnSaveReg
+            // 
+            btnSaveReg.BackColor = SystemColors.ActiveCaption;
+            btnSaveReg.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSaveReg.Location = new Point(372, 216);
+            btnSaveReg.Name = "btnSaveReg";
+            btnSaveReg.Size = new Size(311, 28);
+            btnSaveReg.TabIndex = 18;
+            btnSaveReg.Text = "Resultados de las operaciones con vectores";
+            btnSaveReg.UseVisualStyleBackColor = false;
+            btnSaveReg.Click += btnSaveReg_Click;
+            // 
+            // error1
+            // 
+            error1.ContainerControl = this;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(376, 298);
+            label15.Name = "label15";
+            label15.Size = new Size(116, 15);
+            label15.TabIndex = 26;
+            label15.Text = "Resta de los vectores";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(376, 333);
+            label16.Name = "label16";
+            label16.Size = new Size(172, 15);
+            label16.TabIndex = 27;
+            label16.Text = "Producto punto de los vectores";
+            // 
+            // txtAdd
+            // 
+            txtAdd.Location = new Point(563, 261);
+            txtAdd.Name = "txtAdd";
+            txtAdd.Size = new Size(116, 23);
+            txtAdd.TabIndex = 28;
+            // 
+            // txtSubstract
+            // 
+            txtSubstract.Location = new Point(563, 295);
+            txtSubstract.Name = "txtSubstract";
+            txtSubstract.Size = new Size(116, 23);
+            txtSubstract.TabIndex = 29;
+            // 
+            // txtPointProduct
+            // 
+            txtPointProduct.Location = new Point(563, 330);
+            txtPointProduct.Name = "txtPointProduct";
+            txtPointProduct.Size = new Size(116, 23);
+            txtPointProduct.TabIndex = 30;
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Location = new Point(376, 264);
+            label17.Name = "label17";
+            label17.Size = new Size(118, 15);
+            label17.TabIndex = 31;
+            label17.Text = "Suma de los vectores";
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(834, 333);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(75, 23);
+            btnClear.TabIndex = 32;
+            btnClear.Text = "Nuevo";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(949, 450);
+            Controls.Add(btnClear);
+            Controls.Add(label17);
+            Controls.Add(txtPointProduct);
+            Controls.Add(txtSubstract);
+            Controls.Add(txtAdd);
+            Controls.Add(label16);
+            Controls.Add(label15);
+            Controls.Add(btnSaveReg);
+            Controls.Add(btnExit);
+            Controls.Add(txtZ2);
+            Controls.Add(txtY2);
+            Controls.Add(txtX2);
+            Controls.Add(txtZ1);
+            Controls.Add(txtY1);
+            Controls.Add(txtX1);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(label9);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)error1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox txtX1;
+        private TextBox txtY1;
+        private TextBox txtZ1;
+        private TextBox txtZ2;
+        private TextBox txtY2;
+        private TextBox txtX2;
+        private Button btnExit;
+        private Button btnSaveReg;
+        private ErrorProvider error1;
+        private Label label14;
+        private TextBox txtPointProduct;
+        private TextBox txtSubstract;
+        private TextBox txtAdd;
+        private Label label16;
+        private Label label15;
+        private Button btnAdd;
+        private Label label17;
+        private Button btnClear;
+    }
+}
